9554614ed7ae32e7f476cdb9ae64fbd41587b617
Moringa School <lizdmukami@gmail.com>
2022-11-23 21:29:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+    return array.slice(0,2) 
+}
+
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const returnLastTwoDrivers = (array) => {
+    return array.slice(2)
+}
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (interger) => {
+    return (function fareMultiplier() {
+        return interger ** 2
+    })
+}
+
+const fareDoubler = (createFareMultiplier) => {
+    return createFareMultiplier * 2
+}
+
+const fareTripler = (createFareMultiplier) => {
+    return createFareMultiplier * 3
+}
+
+const selectDifferentDrivers = (array, fun) => fun(array)
